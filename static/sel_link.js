526694ce708ef367a95350d6f64929f667d72ab5
Aveline Cacciatore <70073218+AvyTheBird@users.noreply.github.com>
2025-11-04 19:34:47 -0300

    typed.js

--- static/sel_link.js
+++
@@ -1,9 +0,0 @@
-//const link = document.querySelector('.menu-link');
-const linkList = document.querySelectorAll('.menu-link');
-
-linkList.forEach(link =>{
-    link.addEventListener('click', () => {
-        document.querySelector('.link-selected')?.classList.remove('link-selected');
-        link.classList.add('link-selected');
-    });
-});
